19881fa9ba4844c4c4a74eb70293276b9e759f96
Shailesh Gupta <shaileshgupta596@gmail.com>
2024-01-18 00:44:55 +0530

    Commit3 : Added new Navbar .. Still WIP

--- backend/wauthentication/views.py
+++ backend/wauthentication/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def HomePage(request, *args, **kwargs):
-    return HttpResponse("SEcommerce")
+    template_name = 'base.html'
+    return render(request=request, template_name=template_name, context={})
 
