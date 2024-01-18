5f278144d124091f2216de858a8cac1ef957620e
Shailesh Gupta <shaileshgupta596@gmail.com>
2024-01-18 22:53:40 +0530

    commit4 : Navbar Ready

--- backend/wauthentication/urls.py
+++ backend/wauthentication/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from wauthentication import views
 
+appname= "wauthentication"
+
 urlpatterns = [
     path('', views.HomePage, name="homepage"),
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    
 ]
--- backend/wauthentication/views.py
+++ backend/wauthentication/views.py
@@ -6,3 +6,13 @@ def HomePage(request, *args, **kwargs):
     template_name = 'base.html'
     return render(request=request, template_name=template_name, context={})
 
+
+def register(request, *args, **kwargs):
+    template_name = 'wauthentication/register.html'
+    return render(request=request, template_name=template_name, context={})
+
+
+def login(request, *args, **kwargs):
+    template_name = 'wauthentication/login.html'
+    return render(request=request, template_name=template_name, context={})
+
