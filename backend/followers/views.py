9dab094e78a034b7d803c6b369e54a6b8ec70bc6
Shailesh Gupta <shaileshgupta596@gmail.com>
2024-02-02 10:32:13 +0530

    02-02-2024 : start: followe following feature implementation

--- backend/followers/views.py
+++ backend/followers/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from followers.models import Follower
+from socialapp.models import User
 # Create your views here.
 
 
-@login_required
+@login_required(login_url="/wauthentication/login/")
 def get_followers_following_count_view(request, user_id=None, *args, **kwargs):
     template_name = "userprofile/profile-follower-post.html"
     context = {}
@@ -18,10 +19,30 @@ def get_followers_following_count_view(request, user_id=None, *args, **kwargs):
         "following_count":following_count,
         "post_count": post_count
     }
-    print("rw")
     return render(request=request, template_name=template_name, context=context)
 
+
+@login_required(login_url="/wauthentication/login/")
 def follower_request_list_view(request, *args, **kwargs):
     template_name = "followers/request-list.html"
+    context = {}
+    user = request.user
+    user = User.objects.get(id=user.id)
+
+    if request.method == "POST":
+        requested_follower_id = request.POST.get('follwer_id')
+        follow_object = user.followers.all().filter(status="P").get(follower_id__id = requested_follower_id)
+        follow_object.status = "A"
+        follow_object.save()
+
+    request_list = user.followers.all().filter(status="P")
+    # print(request_list[0].follower_id.userextradetail_set.get().profile_image.url)
+    context = {"objects": request_list}
+    return render(request=request, template_name=template_name, context=context)
+
+
+@login_required(login_url="/wauthentication/login/")
+def follower_request_accept_view(request, *args, **kwargs):
+    template_name = ""
     context = {}
     return render(request=request, template_name=template_name, context=context)
